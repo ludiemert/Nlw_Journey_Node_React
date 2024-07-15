0e0a7fd740d25ad1c7601ef78ef818818e5f23c9
ludiemert <lucianadiemert@gmail.com>
2024-07-15 10:57:59 -0300

    add ajustes no frontend

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,8 +1,28 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen flex items-center justify-center">
+
+      <div className="max-w-3xl px-6 text-center space-y-10">
+        <p className="text-zinc-300 text-lg">
+          Convide seus amigos e planeje sua próxima viagem!
+          </p>
+
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl">
+            <input type="text" placeholder="Where are you going? " />
+            <input type="text" placeholder="When??⏳"/>
+
+            <button>Continue</button>
+          </div>
+
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          com nossos <a className="text-zinc-300 underline" href="#"> termos de uso </a> e <a className="text-zinc-300 underline"  href="#"> políticas de privacidade. </a>
+          </p>
+
+      </div>
+
+    </div>
+
   )
 }
 
