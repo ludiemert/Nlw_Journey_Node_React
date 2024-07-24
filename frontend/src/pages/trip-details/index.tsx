6988d12ebb994f159fc2d628e9a5402c96afab7c
lubeabstracto <luciana@beabstracto.eu>
2024-07-24 10:45:40 -0300

    add componente next page

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,19 +1,19 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
-    <div className="max-w-6xl px-6 py-10 mx-auto ">
-      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape ">
+    <div className="max-w-6xl px-6 py-10 mx-auto space-y-8 ">
+      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between ">
         <div className="flex items-center gap-2">
           <MapPin className="size-5 text-zinc-400" />
-          <span className="text-lg text-zinc-100">Florianópolis, Brasil</span>
+          <span className=" text-zinc-100">Florianópolis, Brasil</span>
         </div>
 
 
         <div className="flex items-center gap-5">
           <div className="flex items-center gap-2">
             <Calendar className="size-5 text-zinc-400" />
-            <span className="text-lg text-zinc-100">17 a 23 August</span>
+            <span className=" text-zinc-100">17 a 23 August</span>
           </div>
 
           <div className="w-px h-6 bg-zinc-800" />
@@ -24,6 +24,24 @@ export function TripDetailsPage() {
           </button>
         </div>
       </div>
+
+      <main className="flex gap-16">
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Activities</h2>
+
+            <button className=" bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            < Plus className=" size-5 text-lime-950" />
+              Register Activity
+            </button>
+
+          </div>
+        </div>
+        <div className="w-80">
+
+        </div>
+
+      </main>
     </div>
 
   )
