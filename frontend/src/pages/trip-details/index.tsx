d98271acfcfab97d294b97e580d20eb16cd07d49
ludiemert <lucianadiemert@gmail.com>
2024-07-24 14:39:29 -0300

    add componente next page

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,5 +1,6 @@
-import { Calendar, CircleCheck, CircleCheckIcon, CircleDashed, Link2,  MapPin, Plus, Settings2, Tag, User, UserCogIcon, X } from "lucide-react";
+import { Calendar, CircleCheck, CircleCheckIcon, CircleDashed, Link2, MapPin, Plus, Settings2, Tag, UserCogIcon, X } from "lucide-react";
 import { useState } from "react";
+import { CreateActivityModal } from "./create-activity-modal";
 
 
 
@@ -17,7 +18,7 @@ export function TripDetailsPage() {
 
 
 
-  
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8 ">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between ">
@@ -95,9 +96,9 @@ export function TripDetailsPage() {
               <div className="flex items-center justify-between gap-4">
                 <div className="space-y-1.5">
                   <span className="block font-medium text-zinc-100">AirBnB reservation</span>
-                   <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
                     https://www.airbnb.com.br/rooms/1047000111212333333221123322
-                    </a>
+                  </a>
                 </div>
                 <Link2 className="text-zinc-400 size-5 shrink-0" />
               </div>
@@ -105,9 +106,9 @@ export function TripDetailsPage() {
               <div className="flex items-center justify-between gap-4">
                 <div className="space-y-1.5">
                   <span className="block font-medium text-zinc-100">Rules of the house</span>
-                   <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                   https://www.notion.com/pages/104700047000111212333333221123322
-                    </a>
+                  <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                    https://www.notion.com/pages/104700047000111212333333221123322
+                  </a>
                 </div>
                 <Link2 className="text-zinc-400 size-5 shrink-0" />
               </div>
@@ -119,17 +120,17 @@ export function TripDetailsPage() {
             </button>
           </div>
           <div className="w-full h-px bg-zinc-800" />
-          
-          
+
+
           <div className="space-y-6">
             <h2 className="font-semibold text-xl">Guests</h2>
             <div className="space-y-5">
               <div className="flex items-center justify-between gap-4">
                 <div className="space-y-1.5">
                   <span className="block font-medium text-zinc-100">Jessica White</span>
-                   <span className="block text-sm text-zinc-400 truncate">
-                   jessica.white44@yahoo.com
-                    </span>
+                  <span className="block text-sm text-zinc-400 truncate">
+                    jessica.white44@yahoo.com
+                  </span>
                 </div>
                 <CircleDashed className="text-zinc-400 size-5 shrink-0" />
               </div>
@@ -137,9 +138,9 @@ export function TripDetailsPage() {
               <div className="flex items-center justify-between gap-4">
                 <div className="space-y-1.5">
                   <span className="block font-medium text-zinc-100">Dr. Rita Pacocha</span>
-                   <span className="block text-sm text-zinc-400 truncate">
-                   lacy.stiedemann@gmail.com
-                    </span>
+                  <span className="block text-sm text-zinc-400 truncate">
+                    lacy.stiedemann@gmail.com
+                  </span>
                 </div>
                 <CircleCheckIcon className="text-zinc-400 size-5 shrink-0" />
               </div>
@@ -157,48 +158,9 @@ export function TripDetailsPage() {
       </main>
 
       {isCreateActivityModalOpen && (
-            <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-            <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-              <div className="space-x-2">
-                <div className=" flex items-center justify-between">
-                  <h2 className="text-lg font-semibold">Register activity</h2>
-                  <button type="button" onClick={closeCreateActivityModal}>
-                    <X className="size-5 text-zinc-400" />
-                  </button>
-                </div>
-      
-                <p className=" text-sm text-zinc-400">
-                All guests can view the activities.
-                   </p>
-              </div>
-      
-              <form className=" space-y-3">
-      
-                <div className="h-14 px-4 bg-zinc-950 border:bg-zinc-800 rounded-lg flex items-center gap-2">
-                  <Tag className="text-zinc-400 size-5" />
-                  <input
-                    name="title"
-                    placeholder="What is the activity?"
-                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
-                </div>
-
-      
-                <div className="h-14 flex-1 px-4 bg-zinc-950 border:bg-zinc-800 rounded-lg flex items-center gap-2">
-                  <Calendar className="text-zinc-400 size-5" />
-                  <input
-                    type="datetime-local"
-                    name="occurs_at"
-                    placeholder="Date and time of the activity"
-                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
-                </div>
-
-                  
-                <button type="submit" className=" bg-lime-300 w-full justify-center text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400">
-                Save activity
-                </button>
-              </form>            
-            </div>      
-          </div>
+        <CreateActivityModal
+        closeCreateActivityModal={closeCreateActivityModal}
+        />
       )}
 
     </div>
